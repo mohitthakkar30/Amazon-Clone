32a59893c9c0c4882ec1a27a11db7bb183023aa9
Mohit Thakkar <thakkarmohit90@gmail.com>
2023-08-31 15:55:43 +0530

    LFG 🚀

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAVN5MACiQmZ26vyhA-1crr9iRjpLdIT6o",
+    authDomain: "fir-a2d4f.firebaseapp.com",
+    projectId: "fir-a2d4f",
+    storageBucket: "fir-a2d4f.appspot.com",
+    messagingSenderId: "176959069528",
+    appId: "1:176959069528:web:fdda19dbb62ecdb2a1d92d",
+    measurementId: "G-V187TPNF9J"
+  };
---
+++ src/pages/checkout.js
@@ -0,0 +1,14 @@
+import Header from "../components/header";
+
+
+function Checkout(){
+
+    return (
+    <div className="bg-gray-100">
+        <Header/>
+    </div>
+    )
+
+}
+
+export default Checkout;
